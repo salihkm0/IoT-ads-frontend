ba714d941fe7a36e924d94e77933a3e05f3b6295
Muhammed Salih Km <salihkm000@gmail.com>
2025-01-10 15:58:14 +0530

    side bar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div className='w-[250px] hidden md:block pt-[70px] z-40 border h-[100vh] fixed top-0 left-[-1px] bg-gray-500 p-4 shadow-md'>
+        sidebar
+    </div>
+  )
+}
--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Container = ({ children, className }) => {
   return (
     <div
-      className={`my-[70px] mx-[20px] md:mx-[30px] md:my-[80px] lg:mx-[40px] xl:mx-[50px] ${
+      className={`my-[70px] ml-[20px] md:ml-[250px]  ${
         className || ""
       }`}
     >
